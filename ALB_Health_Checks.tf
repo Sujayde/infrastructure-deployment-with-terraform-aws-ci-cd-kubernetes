53eb712457933c06a52b44fadfa7265880c652da
Sujay De <47351835+Sujayde@users.noreply.github.com>
2025-09-02 19:37:04 +0900

    ALB_Health_Checks.tf

---
+++ ALB_Health_Checks.tf
@@ -0,0 +1,11 @@
+resource "aws_lb_target_group" "app_tg" {
+  # ...
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    matcher             = "200"
+  }
+}
